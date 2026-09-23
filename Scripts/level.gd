0f5c887ec135af50ec4365f138f2b5379f66f38d
Lýz Lissade <147095077+Lyzurly@users.noreply.github.com>
2026-09-23 13:42:55 -0500

    fade in and out is crisp af

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -2,7 +2,29 @@ class_name Level extends Node2D
 static var ref: Level
 func _init() -> void:
 	ref = self
+	
+var _tween: Tween
+const _FADE_SPEED: float = .5
+
 
 func darken(indeed:bool) -> void:
-	if indeed:
-		modulate = Color("Black")
+	if _tween:
+		_tween.kill()
+	
+	_tween = create_tween()
+	var color: Color = \
+		Color("Black") if indeed else Color("White")
+	var delay: float = \
+		0. if indeed else .75
+	
+	_tween.tween_property(
+		self,"modulate",
+		color,
+		_FADE_SPEED
+	).set_delay(delay)
+	_tween.finished.connect(_on_darkened,CONNECT_ONE_SHOT)
+
+func _on_darkened() -> void:
+	darken(false)
+	
+	
