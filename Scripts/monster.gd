d4c8f14d53231011134ead4d6a842165802e12c3
Lýz Lissade <147095077+Lyzurly@users.noreply.github.com>
2026-09-24 10:07:43 -0500

    robot monster implemented

--- Scripts/monster.gd
+++ Scripts/monster.gd
@@ -7,13 +7,15 @@ const SPEED: float = 50.
 
 func _ready() -> void:
 	%Area2D.body_entered.connect(_on_body_entered)
+	%AnimatedSprite2D.play("run")
+	
 	
 func _physics_process(delta: float) -> void:
 	global_position.x = move_toward(global_position.x,Player.ref.global_position.x,delta * SPEED)
 	if Player.ref.global_position < global_position:
-		%AnimatedSprite2D.flip_h = true
-	else:
 		%AnimatedSprite2D.flip_h = false
+	else:
+		%AnimatedSprite2D.flip_h = true
 
 func _on_body_entered(body:PhysicsBody2D) -> void:
 	if body is Player:
